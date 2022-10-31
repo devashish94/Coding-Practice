2c88625e9ecce035351534ac42955c2dbccd9364
devahish94 <roy.devashish94@gmail.com>
2022-11-01 00:52:59 +0530

    only one file changed

--- Cpp-Coding/a.cpp
+++ Cpp-Coding/a.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
+#define does_this_work ios_base::sync_with_stdio(false);cin.tie(NULL)
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
 using namespace std;
 
 void solve() {
-    int n, k; cin >> n >> k;
-    if (k == 0) {
-        cout << n << "\n";
+    int n; cin >> n;
+    int start = 0, end = n - 1;
+    int input[n];
+    for (int i = 0; i < n; i++) {
+        cin >> input[i];
     }
-    cout << n % k << "\n";
+    for (int i = 0; start <= end && i < n;) {
+        if (i % 2 == 0) {
+            cout << input[start] << " ";
+            i++;
+            start++;
+        } else {
+            cout << input[end] << " ";
+            i++;
+            end--;
+        }
+    }
+    cout << "\n";
 }
 
 int main() {
+    does_this_work;
     int t; cin >> t;
     while (t--) {
         solve();
