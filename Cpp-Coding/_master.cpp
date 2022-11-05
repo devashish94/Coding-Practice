725ac735a3e6a4235849f7420aea49b6315748ec
devahish94 <roy.devashish94@gmail.com>
2022-11-05 12:50:54 +0530

    did some codeforces

--- Cpp-Coding/_master.cpp
+++ Cpp-Coding/_master.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <utility>
+#include <vector>
 #define does_this_work ios_base::sync_with_stdio(false);cin.tie(NULL)
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx2,tune=native")
@@ -7,10 +9,17 @@ using namespace std;
 
 void solve() {
 	int n; cin >> n;
-	for (int i = 0; i < n; i++) {
-		cout << 69 << " ";
+	int a = 0, b = (3 * n) - 1, count = 0;
+	vector<pair<int, int>> arr;
+	for (; a < b;) {
+		arr.push_back(make_pair(a + 1, b + 1));
+		a += 3; b -= 3;
+		count++;
 	}
-	cout << "\n";
+	cout << count << "\n";
+	for (auto i : arr) {
+		cout << i.first << " " << i.second << "\n";
+	}
 }
 
 int main() {
