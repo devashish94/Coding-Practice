5cfb30743bc43b33db8abc6f10d8426964be0375
devahish94 <roy.devashish94@gmail.com>
2022-11-13 01:28:14 +0530

    did absolutely nothing today

--- Java-Coding/IDE.java
+++ Java-Coding/IDE.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 class IDE {
     public static void main(String[] args) {
-
+        int x = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello world");
+        }
     }
 }
