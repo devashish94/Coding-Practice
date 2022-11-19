9ad16d8d72800577a02d03b732ee28a3f7889cb8
devahish94 <roy.devashish94@gmail.com>
2022-11-19 20:55:41 +0530

    started the dsa lab project

--- Java-Coding/IDE.java
+++ Java-Coding/IDE.java
@@ -1,7 +1,37 @@
 import java.util.Scanner;
 
-class IDE {
+public final class IDE {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int x = read.nextInt();
+        while (x --> 0) {
+            String s = read.next();
+            solve(s);
+        }
+    }
 
+    public static void solve(String s) {
+        int left = 0;
+        StringBuilder x = new StringBuilder();
+        for (int i = 0; i < Math.ceil(s.length() / 3.0) + 1; i++) {
+            x.append("Yes");
+        }
+        String t = x.toString();
+
+        while (s.charAt(0) != t.charAt(left)) {
+            left++;
+            if (left == t.length()) {
+                System.out.println("No");
+                return;
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != t.charAt(left)) {
+                System.out.println("No");
+                return;
+            }
+            left++;
+        }
+        System.out.println("Yes");
     }
 }
