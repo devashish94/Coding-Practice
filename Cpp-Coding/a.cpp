7971a854e7557c4ea773e34b09af71d982d15f5d
devashish94 <roy.devashish94@gmail.com>
2023-01-14 13:35:53 +0530

    no idea

--- Cpp-Coding/a.cpp
+++ Cpp-Coding/a.cpp
@@ -1,42 +1,15 @@
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int c(int change, int& h, int& f, int& t) {
-    while (change > 0) {
-        if (change >= 100 && h > 0) {
-            change -= 100;
-            h--;
-        } else if (change >= 50 && f > 0) {
-            change -= 50;
-            f--;
-        } else if (change >= 25 && t > 0) {
-            change -= 25;
-            t--;
-        } else {
-            break;
-        }
+long long power(int N,int R) {
+    long long ans = 1;
+    for (int i = 1; i <= R; i++) {
+        ans  = ans * (N % 1000000007);
     }
-    return change;
+    return ans % 1000000007;
 }
 
 int main() {
-    int n; cin >> n;
-    int h = 0, f = 0, t = 0;
-    while (n--) {
-        int note; cin >> note;
-        if (note == 25) t++;
-        else if (note == 50) f++;
-        else h++;
-        if (note != 25) {
-            int change = note - 25;
-            int check = c(change, h, f, t);
-            if (check != 0) {
-                cout << "NO\n";
-                return 0;
-            }
-        } 
-    }
-    cout << "YES\n";
+    long long n, r; cin >> n >> r;
+    cout << power(n, r) << "\n";
 }
