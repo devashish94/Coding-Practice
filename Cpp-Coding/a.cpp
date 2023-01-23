400c59371ca377fdc8f39cc604e0393dadc89ab7
devashish94 <roy.devashish94@gmail.com>
2023-01-24 00:35:12 +0530

    something i did

--- Cpp-Coding/a.cpp
+++ Cpp-Coding/a.cpp
@@ -1,46 +1,6 @@
-// tough question
 #include <bits/stdc++.h>
 using namespace std;
 
-int negative(vector<int>& arr) {
-    int left = 0, right = arr.size() - 1, neg = -1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] < 0) {
-            neg = mid;
-            left = mid + 1;
-        } else {
-            right = mid - 1;    
-        }
-    }    
-    if (neg == -1) return -1;
-    return neg + 1;
-}
-
-int positive(vector<int>& arr) {
-    int left = 0, right = arr.size() - 1, t = -1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] <= 0) {
-            left = mid + 1;
-        } else {
-            t = mid;
-            right = mid - 1;
-        }
-    }
-    if (t == -1) return -1;
-    return arr.size() - t;
-}
-
-int maximumCount(vector<int>& arr) {
-    int pos = positive(arr), neg = negative(arr);  
-    if (pos == neg && pos == -1) {
-        return 0;
-    }
-    return max(pos, neg);
-}
-
 int main() {
-    vector<int> arr = {5,20,66,1314};
-    cout << "Answer: " << maximumCount(arr) << "\n";
+    
 }
--- Cpp-Coding/leetcode.cpp
+++ Cpp-Coding/leetcode.cpp
@@ -1,31 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-void process(string x, string& t) {
-	for(char i : x) {
-		if (i == '#') {
-			if (!t.empty()) {
-				t.pop_back();
-			} else {
-				continue;
-			}
-			
-		} else {
-			t += i;
-		}
-	}
-}
-
-bool backspaceCompare(string s, string t) {
-	string a = "", b = "";
-	process(s, a);
-	process(t, b);
-	if (a == b) {
-		return true;
-	}
-	return false;
-}
-
 int main() {
-	cout << backspaceCompare("a#c", "b") << "\n";
+	
 }
