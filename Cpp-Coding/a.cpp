c709a193deb811bcecaba6a83f583b96b55fc7b8
devashish94 <roy.devashish94@gmail.com>
2023-01-24 13:54:47 +0530

    codeforces started

--- Cpp-Coding/a.cpp
+++ Cpp-Coding/a.cpp
@@ -1,6 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    
+    int n, coins = 0; cin >> n; 
+    int d[] = {100, 20, 10, 5, 1};
+    for (int i : d) {
+        coins += n / i;
+        n %= i;
+    }
+    cout << coins << "\n";
 }
--- Cpp-Coding/b.cpp
+++ Cpp-Coding/b.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <unordered_map>
-
-int b[100000], n, m, x;
-long long int count_a = 0, count_b = 0;
-std::unordered_map<int, int> map;
 
 int main() {
-    std::cin >> n;
-    for (int i = 0; i < n; i++) { std::cin >> x; map[x] = i; }
-    std::cin >> m;
+    long long int count_a = 0, count_b = 0;
+    int n, m, x;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++) { scanf("%d", &x); a[x] = i; }
+    scanf("%d", &m);
+    int b[m];
     for (int i = 0; i < m; i++) {
-        std::cin >> b[i];
-        x = map[b[i]];
+        scanf("%d", &b[i]);
+        x = a[b[i]];
         count_a += x + 1;
         count_b += n - x;
     }
-    std::cout << count_a << " " << count_b << "\n";
+    printf("%lld %lld\n", count_a, count_b);
 }
