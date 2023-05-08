23929d84b67fc76428baef075ef72df3d7de1187
devashish94 <roy.devashish94@gmail.com>
2023-05-09 02:09:43 +0530

    start hackerrank 30 day

---
+++ Hackerrank-30-day/src/_01_Hello_World.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class _01_Hello_World {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        String inputString = read.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+    }
+}
